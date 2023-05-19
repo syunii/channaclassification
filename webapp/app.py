a57d0b9009881db17f3f0a03b0b133a249b85c91
AdeMadani <adesfrdnmdn@gmail.com>
2023-05-19 20:12:00 +0800

    deploy model streamlit

---
+++ webapp/app.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+import io
+import streamlit as st
+
+from model import ResNet
+from torchvision import transforms
+from torch.utils.data import DataLoader, Subset, random_split
+from PIL import Image
+import os
+
+torch.manual_seed(123)
+
+# Define the class names
+class_names = ['andrao', 'asiatica', 'auranti', 'barca', 'maru', 'stewartii']
+
+# Define the transformation
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Load the model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+state_dict = torch.load('D:/SC4AI/webapp/resnet_model.pt', map_location=device)
+model = ResNet()
+model.load_state_dict(state_dict)
+model = model.to(device)
+
+# Create the predict function
+def predict(image):
+    image = transform(image).unsqueeze(0)
+    output = model(image)
+    _, predicted_idx = torch.max(output, 1)
+    predicted_label = class_names[predicted_idx.item()]
+    return predicted_label
+
+# Create the Streamlit app
+def main():
+    st.title("Image Classifier")
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        if st.button("Predict"):
+            prediction = predict(image)
+            st.write("Prediction:", prediction)
+
+if __name__ == "__main__":
+    main()
---
+++ webapp/model.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+import io
+import streamlit as st
+
+from torchvision import transforms
+from torch.utils.data import DataLoader, Subset, random_split
+from PIL import Image
+import os
+
+torch.manual_seed(123)
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(64, 64, 3)
+        self.layer2 = self._make_layer(64, 128, 3, stride=2)
+        self.layer3 = self._make_layer(128, 256, 3, stride=2)
+        self.layer4 = self._make_layer(256, 512, 3, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+    
+    def _make_layer(self, in_channels, out_channels, num_blocks, stride=1):
+        layers = []
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False))
+        layers.append(nn.BatchNorm2d(out_channels))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(1, num_blocks):
+            layers.append(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
